3d4bebed54349142adc0465d9dd5c270650ebd56
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-11-23 14:33:54 +0530

    TheSwitchStatement Example
    TheSwitchStatement Example

---
+++ Basicfundamentals/TheSwitchStatement.js
@@ -0,0 +1,40 @@
+
+const day = 'uykf';
+
+switch (day) {
+    case 'Monday':
+        console.log("Playing chase!!");
+        break;
+
+    case 'Tuesday':
+        console.log("Playing the PUBG !!");
+        break;
+
+    case 'Wednesday':
+        console.log
+            ("Playing cricket !!");
+        break
+
+
+    case 'Thrusday':
+        console.log("Playing the Lapa chappi !!");
+        break;
+
+
+    case 'Friday':
+        console.log("Playing the Fugdi !!");
+        break;
+
+
+    case 'saturday':
+        console.log("Playing the Dabduba !!");
+        break;
+
+    case 'sunday':
+        console.log("Playing the plapali !!");
+        break;
+
+    default:
+        console.log(" jao Bhad me kuch mat kheloo !");
+
+}
