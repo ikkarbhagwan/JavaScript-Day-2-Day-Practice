b0e299204808dd1bbca847894baa59be281b7253
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-11-23 15:01:49 +0530

    practice
    practice

---
+++ Statement& Expressions/Statement& Expressions.js
@@ -0,0 +1,15 @@
+const age = 10;
+
+age >= 18 ? console.log("This is wine !") : 'this is water';
+console.log("i like to drink water !");
+
+const drink = age >= 14 ? 'wine': 'water ';
+console.log(drink);
+
+let drink2;
+if (age>= 18){
+  drink2 = " this not !"
+}else{
+    drink2 = "is here ";
+}
+console.log(drink2);
