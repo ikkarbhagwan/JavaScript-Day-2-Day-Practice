500e5b145345129bfeee0c3fa3e5a6bc68f4df1e
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-11-28 10:09:21 +0530

    Arrays study
    Arrays study

--- Basicfundamentals/Arrays/Arrays.js
+++ Basicfundamentals/Arrays/Arrays.js
@@ -5,3 +5,23 @@ console.log(friends.length);
 const friendss = new Array ('ikkar ', 'me', 'youi');
 friendss[0]='meme';
 console.log(friendss);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length -2]);
+
+friends[2] = 'Jay sharma';
+console.log(friends);
+
+friends = ['bob','alice'];
+
+
+const firstName = 'Ponas';
+const jons = ['jonas', 'shjarma', 2012- 1999 ,'tecaher',friends];
+
+console.log(jons);
+
+const years = [1999, 1992, 1993, 1994];
+console.log(years);
