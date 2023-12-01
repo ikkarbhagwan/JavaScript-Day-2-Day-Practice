447d3e1e50d89ef35717a13f9a5f1af571124f82
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-01 23:49:47 +0530

    concat
    concat

--- ArrayMethods/ArrayMethods.js
+++ ArrayMethods/ArrayMethods.js
@@ -23,3 +23,29 @@ const removedElement = numbers.pop();
 const numbers = [1, 2, 3];
 const removedElement = numbers.shift();
 // Result: removedElement = 1, numbers = [2, 3]
+
+
+
+// unshift() : Adds one or more elements to the beginning of an array.
+
+
+const numbers = [2, 3];
+numbers.unshift(0, 1);
+// Result: numbers = [0, 1, 2, 3]
+
+
+
+
+// concat() Combines two or more arrays
+const numbers1 = [1, 2];
+const numbers2 = [3, 4];
+const combined = numbers1.concat(numbers2);
+// Result: combined = [1, 2, 3, 4]
+
+
+
+// slice():Extracts a portion of an array without modifying the original.
+
+const numbers = [1, 2, 3, 4, 5];
+const sliced = numbers.slice(1, 4);
+// Result: sliced = [2, 3, 4], numbers = [1, 2, 3, 4, 5]
