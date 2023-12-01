55fb66cb52059440c27ab306b473a0ba4eb4f424
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-01 23:47:14 +0530

    Methods
    Methods

---
+++ ArrayMethods/ArrayMethods.js
@@ -0,0 +1,25 @@
+// push():
+
+// Description: Adds one or more elements to the end of an array.
+
+const numbers = [1, 2, 3];
+numbers.push(4, 5);
+// Result: [1, 2, 3, 4, 5]
+
+
+// pop():
+//  Removes the last element from the end of an array.
+
+
+const numbers = [1, 2, 3];
+const removedElement = numbers.pop();
+// Result: removedElement = 3, numbers = [1, 2]
+
+
+
+// shift():
+// Removes the first element from the beginning of an array.
+
+const numbers = [1, 2, 3];
+const removedElement = numbers.shift();
+// Result: removedElement = 1, numbers = [2, 3]
