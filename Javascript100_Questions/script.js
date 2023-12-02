6c39e80e498870f5b1e27327bbbe414b3d95c813
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-02 09:57:06 +0530

    Operator Precedence
    Operator Precedence

--- Javascript100_Questions/script.js
+++ Javascript100_Questions/script.js
@@ -6,13 +6,55 @@
 
 let num = 55;
 let str = "Name";
-let bool = true;
+let boolean = true;
 let arr = [1, 2, 3, 4];
 let function1 = function () { };
 let obj = { ckye: "value" };
 
 console.log(typeof num);
 console.log(typeof str); console.log
-console.log(typeof bool);
+console.log(typeof boolean);
 console.log(typeof function1);
 console.log(typeof arr);
+console.log(typeof undefinedVariables);
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Type Coercion in Javascript
+
+let number = 55;
+let string = "52";
+let boolean1 = true;
+
+
+console.log(string + number);
+console.log(number == string);
+console.log(boolean == 1);
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Operator Precedence in Javascript
+
+let a = 12;
+let b = 3;
+let c = 4;
+
+let result = a + b * c + (a - b);
+
+
+console.log(result);
+// = 33
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Unary - Binary - Ternary Operators in Javascript
+
+let w = 5;
+
+let w2 = -a;
+console.log(b);
+console.log(++a);
