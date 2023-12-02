a68e913fd8dabdd1e5e46a061ec291cdad504b5c
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-02 08:42:00 +0530

    using type of operator
    using type of operator

---
+++ Javascript100_Questions/script.js
@@ -0,0 +1,18 @@
+
+
+// using type of oprtaor
+
+
+
+let num = 55;
+let str = "Name";
+let bool = true;
+let arr = [1, 2, 3, 4];
+let function1 = function () { };
+let obj = { ckye: "value" };
+
+console.log(typeof num);
+console.log(typeof str); console.log
+console.log(typeof bool);
+console.log(typeof function1);
+console.log(typeof arr);
