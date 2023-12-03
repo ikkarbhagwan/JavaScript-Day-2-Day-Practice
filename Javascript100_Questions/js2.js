ecbca4d8cebc2431af0fe720342d4c962b810a52
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-03 23:46:24 +0530

    Update js2.js

--- Javascript100_Questions/js2.js
+++ Javascript100_Questions/js2.js
@@ -5,3 +5,12 @@ function ResverseString (str){
 
 }
 console.log(ResverseString("hello"));
+
+
+// // Resverse a string 
+
+function ResverseString (str){
+    return str.split('').reverse().join("");
+
+}
+console.log(ResverseString("Bhagwan is here.."));
