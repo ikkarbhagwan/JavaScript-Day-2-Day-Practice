c6ef61696d24db8e9d19e82360c455c538282c15
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-04 00:02:54 +0530

    Practice
    Practice

--- Javascript100_Questions/js2.js
+++ Javascript100_Questions/js2.js
@@ -1,6 +1,6 @@
 // Resverse a string 
 
-function ResverseString (str){
+function ResverseString(str) {
     return str.split('').reverse().join('');
 
 }
@@ -9,8 +9,49 @@ console.log(ResverseString("hello"));
 
 // // Resverse a string 
 
-function ResverseString (str){
+function ResverseString(str) {
     return str.split('').reverse().join("");
 
 }
 console.log(ResverseString("Bhagwan is here.."));
+
+// fizzBuzz
+
+// Print numbers from 1 to n.
+// For multiples of 3, print "Fizz" instead.
+// For multiples of 5, print "Buzz" instead.
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+for (let i = 1; i <= 100; i++) {
+    let output = '';
+
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+    console.log(output || i);
+
+
+}
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    if (i % 5 === 0) {
+        console.log("Buzz");
+
+    } else {
+        console.log("FizzBuzz");
+    }
+}
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
