61f7d005f7799501ecb8218f89332ab7619d7f52
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-04 00:07:15 +0530

    Doing practice
    Doing practice

--- Javascript100_Questions/js2.js
+++ Javascript100_Questions/js2.js
@@ -55,3 +55,15 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+for(let i =1; i<=100; i++){
+
+
+    if(i % 5 ===0 && i % 3 === 0){
+        console.log("FizzBuzz");
+    }else if(i %3 ===0){
+        console.log("Yahoo");
+    }else if(i %3 ===0){
+        console.log("Baaba");
+    }
+}
