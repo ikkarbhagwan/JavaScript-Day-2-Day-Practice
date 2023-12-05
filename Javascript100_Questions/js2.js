802ddc97f9b552fba20bd6681bd2a786d09e2c73
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-05 22:36:34 +0530

    practicing
    practicing

--- Javascript100_Questions/js2.js
+++ Javascript100_Questions/js2.js
@@ -87,3 +87,19 @@ for(let i =1; i<=100; i++){
       // Example
       console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
       
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+    // new challnges \
+  
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
