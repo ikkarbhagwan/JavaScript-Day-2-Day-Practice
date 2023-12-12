019107bb899b44b7e66b939870aace370445b976
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-12 16:41:59 +0530

    Destructing arrays
    Destructing arrays

---
+++ ArrayMethods/Destructing Arrays/indexD.js
@@ -0,0 +1,15 @@
+
+// Destructing Arrays 
+
+const Vehicles = ['tata', 'maruti', 'honda', 'bmw'];
+
+
+// old way 
+
+// const model =Vehicles[0];
+// const engine =Vehicles[1];
+// const speed =Vehicles[2];
+
+const [model, engine, speed] = Vehicles;
+
+
