7beab718f46c2fb353cbea313daec84158f7b800
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-12 17:00:14 +0530

    use in objects
    use in objects

--- Spread Operator/Spread Operator.js
+++ Spread Operator/Spread Operator.js
@@ -6,3 +6,21 @@ const numberTwo = [14,15,16,17];
 const numberCombine = [...numberOne, ...numberTwo];
 
 console.log(numberCombine);
+
+// use in objects 
+
+const myVehicle ={
+    brand : "tata",
+    price : 400000,
+    color : "red"
+}
+const updateMyVehicle ={
+
+    type : "car",
+    year :2021,
+    seson : "winter"
+}
+
+const myUpdateVehicles ={...myVehicle,...updateMyVehicle};
+
+console.log(myUpdateVehicles);
