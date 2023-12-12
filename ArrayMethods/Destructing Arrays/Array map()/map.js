f19e5f1056e78abf16b7ccb78fd594d783f9c56a
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-12 18:19:54 +0530

    map() practice
    map() practice

---
+++ ArrayMethods/Destructing Arrays/Array map()/map.js
@@ -0,0 +1,13 @@
+const newNumber = [10, 11, 12, 13, 14];
+
+const updateNewNumber = newNumber.map(x => x -2);
+
+console.log(updateNewNumber);
+
+// 1
+
+const newNumber1 = [10, 11, 12, 13, 14];
+
+const updateNewNumber1 = newNumber1.map(x => x * 2);
+
+console.log(updateNewNumber1);
