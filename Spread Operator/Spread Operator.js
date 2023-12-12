8b1f89e8ab6d6f7ccf230307801e030318793bb1
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-12 16:54:34 +0530

    The JavaScript spread operator
    The JavaScript spread operator

---
+++ Spread Operator/Spread Operator.js
@@ -0,0 +1,8 @@
+// The JavaScript spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
+
+const numberOne  =[11,12,13,14];
+const numberTwo = [14,15,16,17];
+
+const numberCombine = [...numberOne, ...numberTwo];
+
+console.log(numberCombine);
