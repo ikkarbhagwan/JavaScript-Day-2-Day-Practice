7f70cf7692cc49ff09f829a23747b8e60ee5a976
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-13 12:57:35 +0530

    Define the orderDelivery function in the restaurant object
    Define the orderDelivery function in the restaurant object

--- Destructuring Object/ObjectD.js
+++ Destructuring Object/ObjectD.js
@@ -1,24 +1,39 @@
-let a= 111;
+let a = 111;
 let b = 123;
 
-const obj = {a:23, b:34, c:14};
+const obj = { a: 23, b: 34, c: 14 };
 
-({a,b} = obj);
-console.log(a,b);
+({ a, b } = obj);
+console.log(a, b);
 
+
+
+// Define the orderDelivery function in the restaurant object
+const restaurant = {
+    orderDelivery: function (obj) {
+      console.log(obj);
+    }
+  };
+  
+  // Call the orderDelivery function to place a delivery order
+  restaurant.orderDelivery({
+    time: '22:30',
+    address: 'vai mg road'
+  });
+  
 
 
 // Nested Objects
 const openingHours = {
     fri: {
-      open: 12,
-      close: 13
+        open: 12,
+        close: 13
     }
-  };
-  
-  // Destructuring assignment
-  const { fri: { open, close } } = openingHours;
-  
-  // Logging the extracted values
-  console.log(open, close);
-  
+};
+
+// Destructuring assignment
+const { fri: { open, close } } = openingHours;
+
+// Logging the extracted values
+console.log(open, close);
+
