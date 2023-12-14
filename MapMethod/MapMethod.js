9eb4c6bfb3d6c3452a7013c533ade738373892d3
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-14 08:33:28 +0530

    DataStrucutreMap
    DataStrucutreMap working with ..

---
+++ MapMethod/MapMethod.js
@@ -0,0 +1,10 @@
+const newMethod = new Map([
+    ['Question', 'which is best programming language in the world ?'],
+    [1, 'c'],
+    [2, 'java'],
+    [3, 'Javascript'],
+    ['correct', 3],
+    [true, 'correct'],
+    [false, 'try again!']
+]);
+console.log(newMethod);
