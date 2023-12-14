019ee96df98cec32bb951656b6b7f987ffd748fc
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-14 12:02:52 +0530

    write the code of small Quizz app using ,map () Data strucuture

--- MapMethod/MapMethod.js
+++ MapMethod/MapMethod.js
@@ -1,4 +1,6 @@
-const newMethod = new Map([
+// map data strucuture decalared 
+
+const Question = new Map([
     ['Question', 'which is best programming language in the world ?'],
     [1, 'c'],
     [2, 'java'],
@@ -7,4 +9,17 @@ const newMethod = new Map([
     [true, 'correct'],
     [false, 'try again!']
 ]);
-console.log(newMethod);
+
+console.log(Question);
+
+// Quiz app 
+
+console.log(Question.get('Question'));
+
+for(const [key, value] of Question){
+    if(typeof key === 'number') console.log(`Your answer ${key} : ${value}`)
+};
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(Question.get(Question.get('correct') === answer));
