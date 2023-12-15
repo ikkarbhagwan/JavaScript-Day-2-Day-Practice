2bc6ca27969339d14b4e5c86cc55402d501302fc
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-15 23:46:18 +0530

    update filter

--- filter()/filter().js
+++ filter()/filter().js
@@ -1,9 +1,16 @@
- 
+
 // filters functiojn in js 
 
 
- let numbers = [10,11,12,13,14,15,16];
+let numbers = [10, 11, 12, 13, 14, 15, 16];
 
- let numbers2  =numbers.filter(x => x <= 13);
+let numbers2 = numbers.filter(x => x <= 13);
 
- console.log(numbers2);
+console.log(numbers2);
+
+//  2
+
+const newArray = [1, 2, 3, 4, 5];
+
+const evens = newArray.filter(num => num % 2 === 0);
+console.log(evens);
