779249f20e5944aef1642538cef64357ff1d8f65
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-16 13:54:50 +0530

    Updated

--- MapFunction/MapMethod.js
+++ MapFunction/MapMethod.js
@@ -1,39 +1,49 @@
-// // map data strucuture decalared 
+// map data strucuture decalared 
 
-// const Question = new Map([
-//     ['Question', 'which is best programming language in the world ?'],
-//     [1, 'c'],
-//     [2, 'java'],
-//     [3, 'Javascript'],
-//     ['correct', 3],
-//     [true, 'correct'],
-//     [false, 'try again!']
-// ]);
+const Question = new Map([
+    ['Question', 'which is best programming language in the world ?'],
+    [1, 'c'],
+    [2, 'java'],
+    [3, 'Javascript'],
+    ['correct', 3],
+    [true, 'correct'],
+    [false, 'try again!']
+]);
 
-// console.log(Question);
+console.log(Question);
 
-// // Quiz app 
+// Quiz app 
 
-// console.log(Question.get('Question'));
+console.log(Question.get('Question'));
 
-// for(const [key, value] of Question){
-//     if(typeof key === 'number') console.log(`Your answer ${key} : ${value}`)
-// };
-// const answer = Number(prompt('Your answer'));
-// console.log(answer);
+for(const [key, value] of Question){
+    if(typeof key === 'number') console.log(`Your answer ${key} : ${value}`)
+};
+const answer = Number(prompt('Your answer'));
+console.log(answer);
 
-// console.log(Question.get(Question.get('correct') === answer));
+console.log(Question.get(Question.get('correct') === answer));
 
-// // convert map to Array 
+// convert map to Array 
 
-// console.log([...Question]);
+console.log([...Question]);3
 
 ///////////////////////////
 
-// map 
+// Mapping array elements
+let numbers = [1, 2, 3, 4, 5];
 
-let newArray = [10,20,30,40,50];
+let squaredNumbers = numbers.map((num) => {
+    return num * num;
+});
+
+console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
 
 
-let doubleNum = newArray.map(num => num + 2);
-console.log(doubleNum);
+
+// filter 
+
+let filterTheArray = [10,20,30,40,50,60,70,80,90,100];
+
+let updateFilter = filterTheArray.filter(num => num % 3 === 0);
+console.log(updateFilter);
