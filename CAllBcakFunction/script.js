68ccc422601e60c7541d338a1398ce0faee7041a
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-18 18:09:20 +0530

    Update the code

--- CAllBcakFunction/script.js
+++ CAllBcakFunction/script.js
@@ -3,11 +3,11 @@ const myName = () => {
 }
 
 const greeting = (sayHi, name) => {
-    
-    console.log(sayHi()+ " " + name);
+
+    console.log(sayHi() + " " + name);
 
 }
-greeting(myName,'Thapa Technical');
+greeting(myName, 'Thapa Technical');
 
 // 2
 
@@ -26,14 +26,47 @@ adress(schoolName, "Bright students");
 
 // this is known as callBack function 
 
-function doSomethingAsync(callBack){
+function doSomethingAsync(callBack) {
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log("Task Completed !!");
 
         callBack();
-        
-    }),4000
 
-    
+    }), 4000
+
+
 }
+
+// how callback work 
+// 1.
+setTimeout(function third() {
+
+    console.log("this is asyncronus!!");
+
+
+}, 5000);
+
+
+function first(second) {
+    console.log("this is first ");
+    second();
+}
+first(function second() {
+    console.log("this is second ");
+
+});
+
+// >button>
+// addEventListener to button 
+function attachEventListeners() {
+
+    document.querySelector("#btn").addEventListener('click', function xyz() {
+        console.log("button was clicked!!");
+    })
+
+}
+attachEventListeners() ;
+
+// this forms a closure bcox inner functions have a acces of outer function 
+
