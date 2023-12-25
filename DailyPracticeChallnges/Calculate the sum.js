92e90ddbdaff4377264181e180841e17bcd639e7
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-26 00:07:54 +0530

    // Calculate the sum of numbers in an array of numbers
    // Calculate the sum of numbers in an array of numbers

--- DailyPracticeChallnges/Calculate the sum.js
+++ DailyPracticeChallnges/Calculate the sum.js
@@ -40,3 +40,21 @@ function CelsiusToFharenheit(i){
 let r = CelsiusToFharenheit(i);
 
 console.log(r);
+
+// Calculate the sum of numbers in an array of numbers
+
+function sumArray(ar)
+{
+    let sum1 = 0;
+    
+    for(let i = 0; i < ar.length; i++)
+    {
+        sum1 += ar[i];
+    }
+    
+    return sum;
+}
+
+let ar = [2, 3, -1, 5, 7, 9, 10, 15, 95];
+let sum1 = sumArray(ar);
+console.log(sum);
