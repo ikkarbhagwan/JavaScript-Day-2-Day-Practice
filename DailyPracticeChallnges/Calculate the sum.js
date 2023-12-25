c8d82cbf65861308de5d9f44400c9dae3b600a68
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2023-12-25 23:57:41 +0530

    Coding challenge
    Coding challenge

---
+++ DailyPracticeChallnges/Calculate the sum.js
@@ -0,0 +1,19 @@
+// Coding challenge #5: Calculate the sum of numbers from 1 to 10
+
+let sumOfNumbers = 0;
+
+for (let i = 0; i <= 10; i++) {
+    sumOfNumbers += i;
+}
+
+console.log(sumOfNumbers);
+
+// Coding challenge #6:  Calculate the sum of odd numbers greater than 10 and less than 30
+
+let sum = 0;
+
+for (let i = 11; i <= 30; i + 2) {
+    sum + i;
+}
+
+console.log(sum);
