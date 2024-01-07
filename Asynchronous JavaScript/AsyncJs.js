b49ec75bcd5320b5e1e7f71d088ef626b1460343
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-08 00:23:52 +0530

    update

--- Asynchronous JavaScript/AsyncJs.js
+++ Asynchronous JavaScript/AsyncJs.js
@@ -1,30 +1,43 @@
 
 // Asynchronous JavaScript: 
 
-function featchData(callback){
-    setTimeout (() =>
-    {
-        callback("Data Featched!!! ");
-    },2000);
+function featchData(callback) {
+  setTimeout(() => {
+    callback("Data Featched!!! ");
+  }, 2000);
 }
 featchData(result => {
-    console.log(result);
+  console.log(result);
 });
 
 // PRomises 
 function fetchData() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("Data fetched!");
-      }, 2000);
-    });
-  }
-  
-  fetchData()
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Data fetched!");
+    }, 2000);
+  });
+}
+
+fetchData()
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+//
+
+
+console.log("start of script");
+
+
+setTimeout(function(){
+
+  console.log("first timeout is complete");
+
+
+},2000)
+console.log("end of script .")
