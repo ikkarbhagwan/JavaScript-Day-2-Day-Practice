d3c408997c507d9182521cb410891d21cc400917
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-14 19:30:35 +0530

    Update script.js

--- Javascript100_Questions/script.js
+++ Javascript100_Questions/script.js
@@ -118,3 +118,11 @@ function isPalindrome(str) {
 
     const multiplicationTable = generateMultiplicationTable();
     console.log(multiplicationTable);
+
+    ////////////////////////////////////////
+
+    for(let i = 1; i <= 10; i++)
+{
+let row = `7 * ${i} = ${7 * i}`;   
+    console.log(row);
+}
