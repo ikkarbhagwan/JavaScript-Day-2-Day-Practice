3ca69940f4f13f3487ba1ec4ff7b5dacb1f5ee73
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-20 21:57:51 +0530

    Maximum Subarray

--- DailyPracticeChallnges/Calculate the sum.js
+++ DailyPracticeChallnges/Calculate the sum.js
@@ -864,3 +864,20 @@ console.log(Factorial(5));
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+// 8. Maximum Subarray:
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let maxSum = nums[0];
+let currentSum = nums[0];
+
+for (let i = 1; i < nums.length; i++) {
+  currentSum = Math.max(nums[i], currentSum + nums[i]);
+  maxSum = Math.max(maxSum, currentSum);
+}
+
+console.log(maxSum); // Output: 6
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
