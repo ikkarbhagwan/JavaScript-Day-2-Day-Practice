89f9bdf5c9c05b92601bdb1dc864e6191dc9bc87
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-20 22:17:12 +0530

    // Nested loops:

--- DailyPracticeChallnges/Calculate the sum.js
+++ DailyPracticeChallnges/Calculate the sum.js
@@ -966,7 +966,17 @@ do {
 let array = [10, 20, 30, 40, 50];
 
 for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
+    console.log(array[i]);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
+
+// Nested loops:
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
+
