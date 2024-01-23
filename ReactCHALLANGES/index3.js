6f45fa8ec8455e9c6882f79a2d5221851e9d92d7
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-23 22:51:14 +0530

    new updated

---
+++ ReactCHALLANGES/index3.js
@@ -0,0 +1,15 @@
+// Challenge: Display `JSX is cool!` as an output
+  
+
+function App() {
+
+return (
+
+    <div>
+
+    <h2>Js'x is cool</h2>
+    </div>
+)
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
