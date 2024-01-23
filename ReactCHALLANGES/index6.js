aa52a8cc1ddd26567205ea8f1a50ad8f1ec77c94
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-23 23:15:25 +0530

    completed the 6 th challange

---
+++ ReactCHALLANGES/index6.js
@@ -0,0 +1,13 @@
+
+const myComponent = () => {
+
+    const [value, setvalue] = useState("");
+
+    
+    return (
+        <div>
+            <input onChange={ (e) => setvalue(e.target.value)} value ={value} type="text" placeholder ="Enter Text" />
+            <p>{value}</p>
+        </div>
+    )
+}
