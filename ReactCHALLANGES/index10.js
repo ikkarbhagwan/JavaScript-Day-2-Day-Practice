ca56470a712a52cf9c183ab96a36d2a49a765c9d
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-25 14:41:46 +0530

    again

--- ReactCHALLANGES/index10.js
+++ ReactCHALLANGES/index10.js
@@ -1,21 +1,39 @@
 
-const App(){
+// const App(){
 
-    const [showHide, setShowHide] = useState(true);
+//     const [showHide, setShowHide] = useState(true);
 
+
+//     const handleButton = () =>{
+//         setShowHide(!showHide)
+//     }
+
+
+//     return(
+
+//         <>
+
+//         <button onClick={handleButton}>{showHide ? "show" : "Hide"}</button>
+//         {showHide && <h1>This is Visible </h1>}
+        
+//         </>
+//     )
+// }
+
+const inputFiled = () =>{
+
+    const [showHide , setShowHide] = useState(true);
 
     const handleButton = () =>{
-        setShowHide(!showHide)
-    }
+            setShowHide(!showHide)
 
+    }
 
     return(
 
         <>
-
-        <button onClick={handleButton}>{showHide ? "show" : "Hide"}</button>
-        {showHide && <h1>This is Visible </h1>}
-        
+        <button onClick={handleButton}>{!showHide ? "show" : "hide"}</button>
+        {showHide && <li>This is Visible!</li>}
         </>
     )
 }
