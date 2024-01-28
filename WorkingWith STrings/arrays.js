408272a78d27433718376693c69da3e3f0555afe
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-28 23:24:41 +0530

    Write a JavaScript program to sort the items of an array.

--- WorkingWith STrings/arrays.js
+++ WorkingWith STrings/arrays.js
@@ -72,3 +72,16 @@ console.log(output2 = myColor.join("+"));
 console.log(output2 = myColor.join(","));
 console.log(output2 = myColor.join(" "));
 
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a JavaScript program to sort the items of an array.
+
+var arr1 = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+
+arr1.sort(function(a,b){
+    return a - b;
+
+});
+
+console.log(arr1.join(" "));
+
