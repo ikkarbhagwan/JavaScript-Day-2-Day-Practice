bc18935e971029f0b35837015d6249870971075c
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-28 23:16:10 +0530

    Write a simple JavaScript program to join all elements of the following array into a string.

--- WorkingWith STrings/arrays.js
+++ WorkingWith STrings/arrays.js
@@ -9,3 +9,66 @@ return Array.isArray(input);
 
 console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const array_clone = (input) => {
+
+    return input.slice();
+
+}
+
+console.log(array_clone([1, 2, 4, 0]));
+console.log(array_clone([1, 2, [4, 0]]));
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const first = (n) => {
+
+    return n.slice(1,1);
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+function last(arr,n = 1)
+{
+    if(n<0){
+        return [];
+
+
+    }
+
+    return arr.slice(-n);
+}
+
+console.log(last([7, 9, 0, -2]));       
+console.log(last([7, 9, 0, -2], 3));    
+console.log(last([7, 9, 0, -2], 6));
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a simple JavaScript program to join all elements of the following array into a string.
+
+const myColor = ["Red", "Green", "White", "Black"];
+
+
+console.log(output2 = myColor.join("+"));
+console.log(output2 = myColor.join(","));
+console.log(output2 = myColor.join(" "));
+
