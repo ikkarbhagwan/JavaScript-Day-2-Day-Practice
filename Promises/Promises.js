449ded4e954f7a21f96ad5e9a86e3415d25b8889
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-28 12:32:36 +0530

    promise second example

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -1,27 +1,46 @@
-const myPromise = new Promise((resolve,reject) =>{
+// const myPromise = new Promise((resolve,reject) =>{
 
 
-    setTimeout(() =>{
-        const success = true;
+//     setTimeout(() =>{
+//         const success = true;
 
-        if(success){
-            resolve("Opration done");
+//         if(success){
+//             resolve("Opration done");
 
-        }else{
-            reject("opration fail");
-
-
-        }
+//         }else{
+//             reject("opration fail");
 
 
-    },2000)
+//         }
 
-myPromise
-.then( (result) =>{
-    console.log("success: ", result)
-})
-.catch((error)=>{
-    console.log("success: ", error)
+
+//     },2000)
+
+// myPromise
+// .then( (result) =>{
+//     console.log("success: ", result)
+// })
+// .catch((error)=>{
+//     console.log("success: ", error)
+
+// });
+
+
+// promise second example 
+
+const promiseOne = new Promise((resolve, reject) => {
+
+    // you can perform asyncronus task like : network call etc
+    setTimeout(() => {
+        console.log("You have created a promise!");
+        resolve()
+    }, 1000);
+
 
 });
 
+
+promiseOne.then(function(){
+    console.log("promise consumed");
+
+})
