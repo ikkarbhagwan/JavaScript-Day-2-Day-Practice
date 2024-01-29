77e33e2b35f7268ea5f9678ffcb45766b4900da4
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-29 08:57:53 +0530

    calling method from company object using worker object.

--- PrototypeInheritance/PrototypeInheritance.js
+++ PrototypeInheritance/PrototypeInheritance.js
@@ -1,11 +1,39 @@
 
-let arr = ["akshay", "Damodhar"];
 
 let object = {
-    name : "ikkar Bhagwan",
-    age : 23,
-    city : "Parbhani",
-    getIntro : function(){
-        console.log(this.name + "from" + this.age )
+    name: "ikkar Bhagwan",
+    age: 23,
+    city: "Parbhani",
+    getIntro: function () {
+        console.log(this.name + "from" + this.age)
     }
 }
+
+
+let object2 = {
+    name: "Bhagwan",
+
+}
+
+object2__proto__ = object;
+console.log(object2);
+object.getIntro();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let company = {
+    name: "A",
+    pay: function () {
+      console.log("Paying");
+    },
+  }; //company object
+  let worker = {
+    id: 1,
+    work: function () {
+      console.log("Working");
+    },
+  }; //worker object
+  worker.__proto__ = company; //worker object inherits company object
+  console.log(worker);
+  worker.pay(); // calling method from company object using worker object.
+  
