31bd83a156beaac331b13ba375c1bfeecc21bd06
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-31 22:50:10 +0530

    Calculate the sum of numbers from 1 to 10

--- DailyPracticeChallnges/Calculate the sum.js
+++ DailyPracticeChallnges/Calculate the sum.js
@@ -1638,3 +1638,13 @@ console.log(isPalindrome("hello"));
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
+
+//  Calculate the sum of numbers from 1 to 10
+
+let sumOfNumbers = 0;
+
+for (let i = 0; i <= 10; i++) {
+    sumOfNumbers += i;
+}
+
+console.log(sumOfNumbers);
