d3a8717bbb413e089ba90a707aacae3362a92e35
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-03 21:30:02 +0530

    Write a program that prints the numbers

--- Interview-Practice/index.js
+++ Interview-Practice/index.js
@@ -2,7 +2,7 @@ function checkPalindrome(str) {
 
     const reversedStr = str.split("").reverse().join('');
     return str === reversedStr;
-    
+
 
 }
 
@@ -12,13 +12,41 @@ console.log(checkPalindrome("aca"));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-function checkIsThisString(str){
+function checkIsThisString(str) {
 
     return str.split('').reverse().join("");
-    
+
 
 }
 
 console.log(checkIsThisString("Hello mam"));
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function fizBuzz() {
+    for (let i = 0; i <= 100; i++) {
+
+        if (i % 5 === 0 && i % 3 === 0) {
+            console.log("FizzBizz");
+
+        }else if (i % 5 === 0) {
+            console.log("Fizz");
+
+        } else if (i % 3 === 0) {
+            console.log("Buzz");
+
+        } else {
+            console.log(i);
+
+        }
+
+    }
+
+}
+
+console.log(fizBuzz());
+
 
 
