0f18d89402bf628f00d30b63d0e268a3dafe04ef
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-06 23:35:56 +0530

    callback function

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -299,3 +299,19 @@ function makeSizer(size) {
   
 
   ////////////////////////////////////////////////////////////
+
+
+  function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+  
+  myFirst();
+  mySecond();
+
+
+
+  ////////////////////////////////////////////////////////////
