d31d4ff59e15618ae977b7dd2cca00ae22a63a62
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-06 23:34:29 +0530

    morden practicing of closures using elemnts

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -285,3 +285,17 @@ addMe(20);
 
 
 ////////////////////////////////////////////////////////////
+
+
+function makeSizer(size) {
+    return function () {
+      document.body.style.fontSize = `${size}px`;
+    };
+  }
+  
+  const size12 = makeSizer(12);
+  const size14 = makeSizer(14);
+  const size16 = makeSizer(16);
+  
+
+  ////////////////////////////////////////////////////////////
