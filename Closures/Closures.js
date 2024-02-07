9e46ab6b72086baa1414172b20bf535cb6051ca5
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-07 13:24:47 +0530

    filter
    filter

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -315,3 +315,20 @@ mySecond();
 
 
 ////////////////////////////////////////////////////////////
+
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  function myCalculator(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+  }
+  
+  let result = myCalculator(5, 5);
+  myDisplayer(result);
+
+  ////////////////////////////////////////////////////////////
+
+
+  
--- MapFunction/MapMethod.js
+++ MapFunction/MapMethod.js
@@ -1,49 +1,59 @@
 // map data strucuture decalared 
 
-const Question = new Map([
-    ['Question', 'which is best programming language in the world ?'],
-    [1, 'c'],
-    [2, 'java'],
-    [3, 'Javascript'],
-    ['correct', 3],
-    [true, 'correct'],
-    [false, 'try again!']
-]);
+// const Question = new Map([
+//     ['Question', 'which is best programming language in the world ?'],
+//     [1, 'c'],
+//     [2, 'java'],
+//     [3, 'Javascript'],
+//     ['correct', 3],
+//     [true, 'correct'],
+//     [false, 'try again!']
+// ]);
 
-console.log(Question);
+// console.log(Question);
 
-// Quiz app 
+// // Quiz app 
 
-console.log(Question.get('Question'));
+// console.log(Question.get('Question'));
 
-for(const [key, value] of Question){
-    if(typeof key === 'number') console.log(`Your answer ${key} : ${value}`)
-};
-const answer = Number(prompt('Your answer'));
-console.log(answer);
+// for(const [key, value] of Question){
+//     if(typeof key === 'number') console.log(`Your answer ${key} : ${value}`)
+// };
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
 
-console.log(Question.get(Question.get('correct') === answer));
+// console.log(Question.get(Question.get('correct') === answer));
 
-// convert map to Array 
+// // convert map to Array 
 
-console.log([...Question]);3
+// console.log([...Question]);3
 
-///////////////////////////
+// ///////////////////////////
 
-// Mapping array elements
-let numbers = [1, 2, 3, 4, 5];
+// // Mapping array elements
+// let numbers = [1, 2, 3, 4, 5];
 
-let squaredNumbers = numbers.map((num) => {
-    return num * num;
-});
+// let squaredNumbers = numbers.map((num) => {
+//     return num * num;
+// });
 
-console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+// console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
 
 
 
-// filter 
+// // filter 
 
-let filterTheArray = [10,20,30,40,50,60,70,80,90,100];
+// let filterTheArray = [10,20,30,40,50,60,70,80,90,100];
 
-let updateFilter = filterTheArray.filter(num => num % 3 === 0);
-console.log(updateFilter);
+// let updateFilter = filterTheArray.filter(num => num % 3 === 0);
+// console.log(updateFilter);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let theGivenArray = [1,2,3,4,5,6,5,2,4,8];
+
+const findUniqueValues = theGivenArray.filter( (value,index) => theGivenArray.indexOf(value) === index);
+
+console.log(findUniqueValues);
