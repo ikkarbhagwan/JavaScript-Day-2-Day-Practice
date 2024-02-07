f813155b8cbf7004c58cec5c771201c1726459c9
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-07 14:11:03 +0530

    best Practice

--- ReactCHALLANGES/index6.js
+++ ReactCHALLANGES/index6.js
@@ -3,12 +3,29 @@ const myComponent = () => {
 
     const [value, setvalue] = useState("");
 
-    
+
     return (
         <div>
-            <input onChange={ (e) => setvalue(e.target.value)} value ={value} type="text" placeholder ="Enter Text" />
+            <input onChange={(e) => setvalue(e.target.value)} value={value} type="text" placeholder="Enter Text" />
             <p>{value}</p>
         </div>
     )
 }
 export default myComponent;
+
+
+/// data binding 
+
+const myComponent = () => {
+
+    const [value, setvalue] = useState("");
+
+
+    return (
+        <>
+            <input onChange={(e) => setvalue(e.target.value)} value={value} type="text" placeholder="Enter Text" />
+            <p>{value}</p>
+
+        </>
+    )
+}
