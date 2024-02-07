e09eb806f529caaf8e0c9cca9b8d69f93e92ce6e
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-07 14:17:50 +0530

    using useState

--- ReactCHALLANGES/index7.js
+++ ReactCHALLANGES/index7.js
@@ -6,10 +6,31 @@ const App = () => {
     return (
         <div>
             <h2>Disabled Button Challange</h2>
-            <input type="text" onChange={ (e) => setfirst(e.target.value)}></input>
+            <input type="text" onChange={ (e) => setValue(e.target.value)}></input>
             <button disabled ={value.length < 1 }>Click Me</button>
         </div>
     )
 }
 
 export default App;
+
+
+const findGame = () =>{
+
+    const [value, setValue] = useState('');
+
+
+
+    return (
+
+
+        <>
+        <h2>Disabled Button </h2>
+        <input type="text" onChange={(e) => setValue(e.target.value)}></input>
+        <buttton disabled={value.length<1}>Click me</buttton>    
+
+        </>
+    )
+};
+
+export default findGame;
