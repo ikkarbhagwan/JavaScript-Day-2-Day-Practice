628ae0d3fd8d1045e3cc8b0f9c08ed1ed2f33c27
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-19 21:45:44 +0530

    X5

--- OOP in JavaScript/Prototype.js
+++ OOP in JavaScript/Prototype.js
@@ -21,3 +21,31 @@ bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
 
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function(){
+    this.speed += 10;
+    console.log(`${this.make } is going ${this.speed}km/h.`);
+}
+Car.prototype.brake = function(){
+    this.speed -= 5;
+    console.log(`${this.make } is going ${this.speed}km/h.`);
+}
+
+const X5 = new Car('BMW', 120);
+const volvo = new Car('Mercedes', 200);
+
+volvo.accelerate();
+X5.accelerate();
+volvo.brake();
+X5.accelerate();
+
