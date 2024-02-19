99783ad55e527bb08144ea1cbf96c8981cd14be6
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-19 21:44:21 +0530

    Private & Protected Properties and Methods
    Private & Protected Properties and Methods

--- OOP in JavaScript/Private & Protected Properties and Methods.js
+++ OOP in JavaScript/Private & Protected Properties and Methods.js
@@ -26,4 +26,35 @@ var userOne = new user('thisssss@gmail.com', 'yiyi');
 var userTwo= new user('yamaha@gmail.com', 'pipi');
 
 
+////////////////////////////////////////////////////////////////////////////////////////////
+
+class user {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+        
+    }
+
+    login(){
+        console.log(this.email, 'just logged in!');
+
+    }
+    logout(){
+        console.log(this.name,'just logged off!');
+
+    }
+
+    updateScore(){
+        this.score++;
+        console.log(this.score,'score is updated :');
+
+    }
+}
+
+var userOne = new user('thisssss@gmail.com', 'yiyi');
+var userTwo= new user('yamaha@gmail.com', 'pipi');
+
+
+
 
