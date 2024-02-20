2b5bb0efe672459110afa5e32e3801522fce9d3e
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-20 16:21:02 +0530

    // write a function that return a longest word in sentence

--- Top10-CodingChallanges/Top10-CodingChallanges.js
+++ Top10-CodingChallanges/Top10-CodingChallanges.js
@@ -16,10 +16,30 @@ console.log(reversedString("hello"));
 
 // 2.
 
-function getReversed(str){
+function getReversed(str) {
 
     return str.split("").reverse().join("");
 
 }
 
 console.log(getReversed("papa"));
+
+
+
+// write a function that return a longest word in sentence 
+
+function returnLongestWord(sentence) {
+    const words = sentence.split(" ");
+    let longWord = "";
+
+    for(let word of words){
+        if(word.length > longWord.length){
+            longWord = word;
+        }
+    }
+   return longWord
+
+
+}
+
+console.log(returnLongestWord("i love my india"));
