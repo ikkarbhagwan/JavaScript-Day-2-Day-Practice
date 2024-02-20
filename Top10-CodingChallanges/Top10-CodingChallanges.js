b11de9c1f94462abed79592c82f817ca59431bfc
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-02-20 16:13:26 +0530

    // write a function that reverse of string ?

---
+++ Top10-CodingChallanges/Top10-CodingChallanges.js
@@ -0,0 +1,16 @@
+// write a function that reverse of string ?
+
+function reversedString(str) {
+
+    let reversed = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+
+    }
+    return reversed;
+
+}
+
+console.log(reversedString("hello"));
+
